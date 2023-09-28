c7d412b95a5b73142df9c2f46a21292367587e63
rhossain7 <reedwan.hossan@rootnext.com>
2023-09-28 19:40:55 +0600

    set async before mongo db confic

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const { MONGO_URI } = process.env;
 
-exports.connect = () => {
+exports.connect = async () => {
   // Connecting to the database
-  mongoose
+  await mongoose
     .connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
